92344cb612849a287353c463a29e947e2b5798c3
IsaevaMaria <isaevam2015@gmail.com>
2021-04-01 21:49:43 +0300

    New project

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from data.register import RegisterForm
+import datetime
+from flask import Flask, url_for, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def main():
+    return "HELLO"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login')
+def log():
+    return 'ALL RIGHT'
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/mars_explorer.db")
+    app.run(port=8080, host='127.0.0.1')
+
